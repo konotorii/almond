39c6384b08f63480c024a15dce57dc331282adce
Konotorii <79553360+konotorii@users.noreply.github.com>
2024-06-19 14:21:41 -0400

    feat: API Routes

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,14 @@ import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
-
+import {Cache} from "./helpers/cache";
 import * as middlewares from './middlewares';
-import api from './api';
-import MessageResponse from './interfaces/MessageResponse';
+import {parse} from 'express-useragent';
+import proxyPrivateDownload from "./helpers/proxyDownload";
+import {send} from "micro";
+import checkAlias from "./helpers/aliases";
+import {valid, compare} from 'semver';
+import urlHelpers from 'url';
 
 require('dotenv').config();
 
@@ -16,13 +20,193 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.get<{}, MessageResponse>('/', (req, res) => {
-  res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
-  });
+const cache = new Cache({
+    token: process.env.TOKEN || "",
+    repository: process.env.REPOSITORY || "",
+    account: process.env.ACCOUNT || "",
+    url: process.env.URL || "",
+    pre: process.env.PRE || "",
+})
+
+const shouldProxyPrivateDownload = cache.config.token  && cache.config.token.length > 0
+
+const {loadCache} = cache
+
+app.get('/download', async (req, res) => {
+    const userAgent = parse(req.headers['user-agent'] || "")
+    const params = urlHelpers.parse(req.url, true).query
+    const isUpdate = params && params.update
+
+    let platform: string
+
+    if (userAgent.isMac && isUpdate) {
+        platform = 'darwin'
+    } else if (userAgent.isMac && !isUpdate) {
+        platform = 'dmg'
+    } else if (userAgent.isWindows) {
+        platform = 'exe'
+    } else {
+        platform = ''
+    }
+
+    // Get the latest version from the cache
+    const {platforms} = await loadCache()
+
+    const findPlatform = platforms.find((v) => v.platform == platform)
+
+    if (!platform || !platforms || !findPlatform) {
+        send(res, 404, 'No download available for your platform!')
+        return
+    }
+
+    if (shouldProxyPrivateDownload) {
+        await proxyPrivateDownload(findPlatform, cache.config.token, req, res)
+        return
+    }
+
+    res.writeHead(302, {
+        Location: findPlatform.url
+    })
+
+    res.end()
 });
 
-app.use('/api/v1', api);
+app.get('/download/:platform', async (req, res) => {
+    const params = urlHelpers.parse(req.url, true).query
+    const isUpdate = params && params.update
+
+    let {platform} = req.params
+
+    if (platform === 'mac' && !isUpdate) {
+        platform = 'dmg'
+    }
+
+    if (platform === 'mac_arm64' && !isUpdate) {
+        platform = 'dmg_arm64'
+    }
+
+    else platform = ''
+
+    // Get the latest version from the cache
+    const latest = await loadCache()
+
+    // Check platform for appropriate aliases
+    platform = checkAlias(platform)
+
+    const findPlatform = latest.platforms.find((v) => v.platform == platform)
+
+    if (!findPlatform) return send(res, 404, 'Specified platform not found!')
+
+    if (!platform) {
+        send(res, 500, 'The specified platform is not valid')
+        return
+    }
+
+    if (!latest.platforms || !platform) {
+        send(res, 404, 'No download available for your platform')
+        return
+    }
+
+    if (cache.config.token && cache.config.token.length > 0) {
+        await proxyPrivateDownload(platform, cache.config.token, req, res)
+        return
+    }
+
+    res.writeHead(302, {
+        Location: findPlatform.url
+    })
+
+    res.end()
+});
+
+app.get('/update/:platform/:version', async (req, res) => {
+    const {platform: platformName, version} = req.params
+
+    if (!valid(version)) {
+        send(res, 500, {
+            error: 'version_invalid',
+            message: 'The specified version is not SemVer-compatible'
+        })
+
+        return
+    }
+
+    const platform = checkAlias(platformName)
+
+    const findPlatform = cache.config.latest.platforms.find((v) => v.platform == platform)
+
+    if (!findPlatform) {
+        send(res, 500, {
+            error: 'invalid_platform',
+            message: 'The specified platform is not valid'
+        })
+
+        return
+    }
+
+    // Get the latest version from the cache
+    const latest = await loadCache()
+
+    if (!latest.platforms || !findPlatform) {
+        res.statusCode = 204
+        res.end()
+
+        return
+    }
+
+    // Previously, we were checking if the latest version is
+    // greater than the one on the client. However, we
+    // only need to compare if they're different (even if
+    // lower) in order to trigger an update.
+
+    // This allows developers to downgrade their users
+    // to a lower version in the case that a major bug happens
+    // that will take a long time to fix and release
+    // a patch update.
+
+    if (compare(latest.version, version) !== 0) {
+        const {notes, pub_date} = latest
+
+        send(res, 200, {
+            name: latest.version,
+            notes,
+            pub_date,
+            url: shouldProxyPrivateDownload
+                ? `${cache.config.url}/download/${platformName}?update=true`
+                : findPlatform.url
+        })
+
+        return
+    }
+
+    res.statusCode = 204
+    res.end()
+});
+
+app.get('/overview', async (req, res) => {
+
+});
+
+app.get('/releases', async (req, res) => {
+    // Get the latest version from the cache
+    const latest = await loadCache()
+
+    if (!latest.files || !latest.files.releases) {
+        res.statusCode = 204
+        res.end()
+
+        return
+    }
+
+    const content = latest.files.releases
+
+    res.writeHead(200, {
+        'content-length': Buffer.byteLength(content, 'utf8'),
+        'content-type': 'application/octet-stream'
+    })
+
+    res.end(content)
+})
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);

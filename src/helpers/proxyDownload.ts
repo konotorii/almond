f942e1de9c640eee511e370cb5c9ef941b070a2e
Konotorii <79553360+konotorii@users.noreply.github.com>
2024-06-19 14:20:41 -0400

    feat: Proxy Downloader

---
+++ src/helpers/proxyDownload.ts
@@ -0,0 +1,23 @@
+import {send} from 'micro';
+import fetch from 'node-fetch';
+import {Request, Response} from 'express';
+
+export default async function proxyPrivateDownload(asset: any, token: string, req: Request, res: Response) {
+    const redirect = 'manual'
+    const headers: HeadersInit = {Accept: 'application/octet-stream'}
+
+    const {api_url: rawUrl} = asset
+
+    const finalUrl = rawUrl.replace(
+        'https://api.github.com/',
+        `https://${token}@api.github.com/`
+    )
+
+    const assetRes = await fetch(finalUrl, {
+        headers,
+        redirect,
+    })
+
+    res.setHeader('Location', assetRes.headers.get('Location') || "")
+    send(res, 302)
+}

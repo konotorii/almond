92229d7965a26d01ed6e32e3a5d84a769e5daa7b
Konotorii <79553360+konotorii@users.noreply.github.com>
2024-06-24 11:13:03 -0400

    fix: Proxy Downloader sends token through headers instead of inside URL

--- src/helpers/proxyDownload.ts
+++ src/helpers/proxyDownload.ts
@@ -3,16 +3,16 @@ import {Request, Response} from 'express';
 
 export default async function proxyPrivateDownload(asset: any, token: string, req: Request, res: Response) {
     const redirect = 'manual'
-    const headers: HeadersInit = {Accept: 'application/octet-stream'}
+    const headers: HeadersInit = {Accept: 'application/octet-stream', Authorization: `token ${token}`};
 
     const {api_url: rawUrl} = asset
 
-    const finalUrl = rawUrl.replace(
-        'https://api.github.com/',
-        `https://${token}@api.github.com/`
-    )
+    // const finalUrl = rawUrl.replace(
+    //     'https://api.github.com/',
+    //     `https://${token}@api.github.com/`
+    // )
 
-    const assetRes = await fetch(finalUrl, {
+    const assetRes = await fetch(rawUrl, {
         headers,
         redirect,
     })
